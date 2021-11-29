a7bce069bc251018d3811b425ac080523fa5d095
jack <1530382442@qq.com>
2021-11-29 20:47:32 +0800

    项目初始化

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,64 @@
 import { createStore } from 'vuex'
 
+const moduleA = {
+
+    state: {
+        username: "moduleA"
+    },
+    getters: {
+        newName(state) {
+            return state.username + "/////"
+        }
+    },
+    mutations: {
+        updateName(state) {
+            state.username = "moduleAAAAAA"
+        }
+    }
+}
+const moduleB = {
+    namespaced: true,
+    state: {
+        username: "moduleB"
+    },
+    getters: {
+        newName(state) {
+            return state.username + "/////"
+        }
+    },
+    mutations: {
+        updateName(state, payload) {
+            state.username = payload
+        }
+    },
+    actions: {
+        update(ctx, payload) {
+            setTimeout(() => {
+                ctx.commit("updateName", payload)
+            }, 1000)
+
+        }
+    },
+}
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    // state: {
+    //     username: "zjb"
+    // },
+    // mutations: {
+    //     updateName(state, payload) {
+    //         state.username = payload
+    //     }
+    // },
+    // actions: {
+    //     update(ctx, payload) {
+    //         setTimeout(() => {
+    //             ctx.commit("updateName", payload)
+    //         }, 1000)
+
+    //     }
+    // },
+    modules: {
+        moduleA,
+        moduleB
+    }
 })
